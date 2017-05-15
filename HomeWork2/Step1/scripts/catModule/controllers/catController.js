5e689d5e451680135705dd3c975b4d0db316ee08
muqtharbasha <Muqthar_Shaik@epam.com>
2017-05-15 19:53:14 +0530

    CSS design pending for HW2 Step1

--- HomeWork2/Step1/scripts/catModule/controllers/catController.js
+++ HomeWork2/Step1/scripts/catModule/controllers/catController.js
@@ -5,76 +5,45 @@
         .controller('CatClicker', CatClicker);
 
     function CatClicker() {
+
         var vm = this;
+        
         vm.message = "Display List of Cats with Display Area and Increase the Count on Image Click";
-        vm.tick = 'images/tick.png';
-        vm.getCatCount = function (imageId) {
-            angular.forEach(vm.filterResult, function (item) {
-                if (item.id == imageId) {
-                    item.count = item.count + 1;
-                }
-            });
-        };
+        vm.tickImg = 'images/tick.png';
 
-        vm.displayCatData = function (catId) {
-            vm.catResult = vm.catsList.filter(function (item) {
-                if (item.id == catId) {
-                    item.isViewed = true
-                    return item;
-                }
-            });
+        vm.selectedCat = null;
 
-            vm.filterResult = vm.catDisplayData.filter(function (item) {
-                if (item.id == catId) {
-                    return item;
-                }
-            });
+
+        vm.getCatInfo = function (cat) {
+            cat.isViewed = true;
+            vm.selectedCat = cat;
         }
 
-        vm.catsList = [{
-            id: 0,
-            catName: 'Cat 1',
-            isViewed: false
-        }, {
-            id: 1,
-            catName: 'Cat 2',
-            isViewed: false
-        }, {
-            id: 2,
-            catName: 'Cat 3',
-            isViewed: false
-        }, {
-            id: 3,
-            catName: 'Cat 4',
-            isViewed: false
-        }];
-
-        vm.catDisplayData = [{
+        vm.catInfo = [{
             id: 0,
             imageUrl: 'images/cat1.jpg',
             catName: 'Cat 1',
-            count: 0
+            isViewed: false,
+            voteCount: 0
         }, {
             id: 1,
             imageUrl: 'images/cat2.jpg',
             catName: 'Cat 2',
-            count: 0,
-            isViewed: false
+            isViewed: false,
+            voteCount: 0
         }, {
             id: 2,
             imageUrl: 'images/cat3.jpg',
             catName: 'Cat 3',
-            count: 0,
-            isViewed: false
+            isViewed: false,
+            voteCount: 0
         }, {
             id: 3,
             imageUrl: 'images/pic_mountain.jpg',
             catName: 'Cat 4',
-            count: 0,
-            isViewed: false
-
+            isViewed: false,
+            voteCount: 0
         }];
-
     }
 
 })();
