db2b0b7028443e5b6aa40af07666ae06e9403908
Muqthar <muqtharbasha@gmail.com>
2017-05-16 00:57:39 +0530

    completed HW2 step2 task

--- HomeWork1/Step3/scripts/catModule/controllers/catController.js
+++ HomeWork1/Step3/scripts/catModule/controllers/catController.js
@@ -1,60 +1,48 @@
 (function () {
+
     'use strict';
+
     angular
         .module('app')
         .controller('CatClicker', CatClicker);
 
     function CatClicker() {
+
         var vm = this;
         vm.message = "Display List of Cats with Display Area and Increase the Count on Image Click";
 
-        vm.getCatCount = function (imageId) {
-            angular.forEach(vm.res, function (item) {
-                if (item.id == imageId) {
+        vm.selectedCat = null;
+
+        vm.getCatCount = function (catId) {
+            angular.forEach(vm.catsList, function (item) {
+                if (item.id == catId) {
                     item.count = item.count + 1;
                 }
             });
         };
 
-        vm.displayCatData = function (catId) {
-            vm.res = vm.catDisplayData.filter(function (item) {
-                if (item.id == catId) {
-                    return item;
-                }
-            });
+        vm.displayCatData = function (cat) {
+            vm.selectedCat = cat;
         }
 
-        vm.catsList = [{
-            id: 0,
-            catName: 'Cat 1',
-        }, {
-            id: 1,
-            catName: 'Cat 2',
-        }, {
-            id: 2,
-            catName: 'Cat 3',
-        }, {
-            id: 3,
-            catName: 'Cat 4',
-        }];
 
-        vm.catDisplayData = [{
-            id: 0,
+        vm.catsList = [{
+            id: 1,
             imageUrl: 'images/cat1.jpg',
             catName: 'Cat 1',
             count: 0
         }, {
-            id: 1,
+            id: 2,
             imageUrl: 'images/cat2.jpg',
             catName: 'Cat 2',
             count: 0
         }, {
-            id: 2,
+            id: 3,
             imageUrl: 'images/cat3.jpg',
             catName: 'Cat 3',
             count: 0
         }, {
-            id: 3,
+            id: 4,
             imageUrl: 'images/pic_mountain.jpg',
             catName: 'Cat 4',
             count: 0
---
+++ HomeWork2/Step2/scripts/catModule/controllers/catController.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+    angular
+        .module('app')
+        .controller('CatClicker', CatClicker);
+
+    function CatClicker() {
+
+        var vm = this;
+
+        vm.message = "Add a search box and orderBy fields";
+        vm.tickImg = 'images/tick.png';
+
+        vm.selectedCat = null;
+
+
+        vm.getCatInfo = function (cat) {
+            vm.selectedCat = cat;
+        }
+
+        vm.searchCatData = function (catName) {
+            vm.selectedCat = vm.catInfo.filter(function (item) {
+                if (item.catName === catName) {
+                    return item;
+                }
+            })[0];
+        };
+        vm.setOrder = function(sortBy){
+            if(sortBy === 'asc'){
+                vm.order = 'catName';
+            }else{
+                vm.order = '-catName';
+            }
+        }
+
+        vm.catInfo = [{
+            id: 1,
+            imageUrl: 'images/cat1.jpg',
+            catName: 'Cat1'
+        }, {
+            id: 2,
+            imageUrl: 'images/cat2.jpg',
+            catName: 'Cat2'
+        }, {
+            id: 3,
+            imageUrl: 'images/cat3.jpg',
+            catName: 'Cat3'
+        }, {
+            id: 4,
+            imageUrl: 'images/pic_mountain.jpg',
+            catName: 'Cat4'
+        }];
+    }
+
+})();
